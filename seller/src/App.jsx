28cdd30764041488acb8ef35f3afa5da26253cde
SANDEEP NAUTIYAL <138180022+nautiyalsandeep19@users.noreply.github.com>
2025-09-08 13:35:03 +0530

    register added

--- seller/src/App.jsx
+++ seller/src/App.jsx
@@ -1,19 +1,30 @@
-import './App.css'
-import Navbar from './common/Navbar'
-import { Route , Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Footer from '@/common/Footer'
+import "./App.css";
+import Navbar from "./common/Navbar";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "./pages/Home";
+import Footer from "@/common/Footer";
+import Register from "./pages/Auth/Register";
+
 function App() {
+  const location = useLocation();
+
+  // Define routes where Navbar should be hidden
+  const hideNavbarRoutes = ["/register"];
 
   return (
-   <>
-  <Navbar/>
-  <Routes>
-    <Route path='/' element={<Home/>} />
-  </Routes>
-     <Footer/>
-   </>  
-   )
+    <>
+      {/* Render Navbar only if current path is not in hideNavbarRoutes */}
+      {!hideNavbarRoutes.includes(location.pathname) && <Navbar />}
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+
+      {/* Same logic can be applied to Footer if you want */}
+      {!hideNavbarRoutes.includes(location.pathname) && <Footer />}
+    </>
+  );
 }
 
-export default App
+export default App;
