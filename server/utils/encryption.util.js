639d97a0b182cfc7e05cb8caf09757702b3e641d
SANDEEP NAUTIYAL <138180022+nautiyalsandeep19@users.noreply.github.com>
2025-09-18 11:40:58 +0530

    refactor: clean up comments and improve readability in seller registration and encryption utility

--- server/utils/encryption.util.js
+++ server/utils/encryption.util.js
@@ -2,18 +2,16 @@ import crypto from "crypto";
 
 const algorithm = "aes-256-cbc";
 const secretKey =
-  process.env.GSTIN_SECRET_KEY || "12345678901234567890123456789012"; // must be 32 bytes
+  process.env.GSTIN_SECRET_KEY || "12345678901234567890123456789012";
 
-// Encrypt GSTIN
 export const encrypt = (text) => {
-  const iv = crypto.randomBytes(16); // unique initialization vector
+  const iv = crypto.randomBytes(16);
   const cipher = crypto.createCipheriv(algorithm, Buffer.from(secretKey), iv);
   let encrypted = cipher.update(text, "utf8");
   encrypted = Buffer.concat([encrypted, cipher.final()]);
-  return iv.toString("hex") + ":" + encrypted.toString("hex"); // save iv + encrypted data
+  return iv.toString("hex") + ":" + encrypted.toString("hex");
 };
 
-// Decrypt GSTIN (useful when you need original GSTIN later)
 export const decrypt = (text) => {
   const textParts = text.split(":");
   const iv = Buffer.from(textParts.shift(), "hex");
