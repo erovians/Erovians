6cbbe3eb4116b873493e3d6cd2f224deef4f70dc
SANDEEP NAUTIYAL <138180022+nautiyalsandeep19@users.noreply.github.com>
2025-09-19 18:10:12 +0530

    feat: add updateProductFields functionality to handle product updates

--- server/routes/product.route.js
+++ server/routes/product.route.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { addProduct, listProducts } from "../controller/product.controller.js";
+import {
+  addProduct,
+  listProducts,
+  updateProductFields,
+} from "../controller/product.controller.js";
 
 const router = express.Router();
 
 router.post("/add", addProduct);
 
 router.get("/list", listProducts);
+
+router.patch("/update/:productId", updateProductFields);
 
 export default router;
