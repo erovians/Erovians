43d5cbf963e3f83db99aada8ae7ad8eae8e7bd84
SANDEEP NAUTIYAL <138180022+nautiyalsandeep19@users.noreply.github.com>
2025-10-08 15:40:13 +0530

    updated the responsiveness of the list product and the product details components

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ app.use(express.json({ limit: "10mb" }));
 // Enable CORS
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "http://192.168.29.142:5173"],
     credentials: true,
   })
 );
