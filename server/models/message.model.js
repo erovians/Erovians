513d00067d92c20298e985daf234ff535c8e7726
SANDEEP NAUTIYAL <138180022+nautiyalsandeep19@users.noreply.github.com>
2025-11-03 14:46:23 +0530

    feat(Chat): enhance chat functionality by initializing messages array and updating message handling

--- server/models/message.model.js
+++ server/models/message.model.js
@@ -2,11 +2,19 @@ import mongoose from "mongoose";
 
 const messageSchema = new mongoose.Schema(
   {
-    chatId: { type: mongoose.Schema.Types.ObjectId, ref: "Chat" },
     senderId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     text: { type: String },
+    createdAt: { type: Date, default: Date.now },
+  },
+  { _id: false }
+);
+
+const chatSchema = new mongoose.Schema(
+  {
+    members: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    messages: [messageSchema],
   },
   { timestamps: true }
 );
 
-export default mongoose.model("Message", messageSchema);
+export default mongoose.model("Chat", chatSchema);
