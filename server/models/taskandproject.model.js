069d9dfab3b18718865446927eb906fdccc6705b
sandeep nautiyal <nautiyalsandeep19@gmail.com>
2025-11-28 12:52:47 +0530

    Add TaskAndProject model; define schema with title, description, status, and createdBy fields

---
+++ server/models/taskandproject.model.js
@@ -0,0 +1,10 @@
+import mongoose ,{ Schema } from 'mongoose';
+
+
+const TaskAndProjectSchema = new Schema({
+
+    title: { type: String, required: true  },
+    description: { type: String },
+    status: { type: String, enum: ['To Do', 'In Progress', 'Completed'], default: 'To Do' },
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, 
+})
