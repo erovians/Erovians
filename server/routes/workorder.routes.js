e0cc610f7fb7a0a0674ec54bce8eaca2f6f09ad9
SANDEEP NAUTIYAL <138180022+nautiyalsandeep19@users.noreply.github.com>
2025-12-02 17:58:06 +0530

    sum up all the code for 2 december 2025

--- server/routes/workorder.routes.js
+++ server/routes/workorder.routes.js
@@ -8,12 +8,8 @@ import { verifyUser, allowRoles } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.get("/", getWorkOrders);
-router.post("/add", createWorkOrder);
-router.put("/:id", updateWorkOrderStatus);
-
-// router.get("/",verifyUser, allowRoles, getWorkOrders);
-// router.post("/add",verifyUser, allowRoles, createWorkOrder);
-// router.put("/:id",verifyUser, allowRoles, updateWorkOrderStatus);
+router.get("/", verifyUser, allowRoles, getWorkOrders);
+router.post("/add", verifyUser, allowRoles, createWorkOrder);
+router.put("/:id", verifyUser, allowRoles, updateWorkOrderStatus);
 
 export default router;
