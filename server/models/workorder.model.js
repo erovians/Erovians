74af92c72ffc32827168c6f56b0e7fadcbb05797
SANDEEP NAUTIYAL <138180022+nautiyalsandeep19@users.noreply.github.com>
2025-12-02 12:50:31 +0530

    created the backend of workorder model , routes and schema  with zod validation

--- server/models/workorder.model.js
+++ server/models/workorder.model.js
@@ -1,9 +1,14 @@
 import mongoose from "mongoose";
 
 const WorkOrderSchema = new mongoose.Schema({
+  sellerId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Seller",
+    required: true,
+  },
+
   wo_number: { type: String, required: true },
   so_number: { type: String, required: true },
-
   machine: { type: String, required: true },
 
   status: {
