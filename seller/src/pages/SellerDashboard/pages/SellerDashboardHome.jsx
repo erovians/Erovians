fb8e9ea7dbccc7d81e7ca0137f2270902e73790d
SANDEEP NAUTIYAL <138180022+nautiyalsandeep19@users.noreply.github.com>
2025-12-04 15:20:47 +0530

    created the buyer file structure and installed the vite to create it

--- seller/src/pages/SellerDashboard/pages/SellerDashboardHome.jsx
+++ seller/src/pages/SellerDashboard/pages/SellerDashboardHome.jsx
@@ -50,44 +50,43 @@ const SellerDashboardHome = () => {
       </div>
 
       {/* 🔻 RESPONSIVE ROW OF CARDS */}
-<div
-  className="
+      <div
+        className="
     grid gap-6
     grid-cols-1
     md:grid-cols-3  /* 2 cards: chart = 2 parts, pie chart = 1 part */
   "
->
-  {/* Seller Chart — take 2 columns */}
-  <div
-    className="
+      >
+        {/* Seller Chart — take 2 columns */}
+        <div
+          className="
       bg-white rounded shadow
       p-4 sm:p-5 md:p-6
       h-[380px] sm:h-[430px] md:h-[480px]
       flex flex-col hover:-translate-y-1 transition
       md:col-span-2  /* ⭐ gives chart more width */
     "
-  >
-    <SellerChart />
-  </div>
+        >
+          <SellerChart />
+        </div>
 
-  {/* Pie Chart — take 1 column */}
-  <div
-    className="
+        {/* Pie Chart — take 1 column */}
+        <div
+          className="
       bg-white rounded shadow
       p-4 sm:p-5 md:p-6
       h-[380px] sm:h-[430px] md:h-[480px]
       flex flex-col hover:-translate-y-1 transition
     "
-  >
-    <h4 className="text-center font-semibold text-gray-800 mb-2">
-      Top Selling Products
-    </h4>
-    <div className="flex-1">
-      <SellerPieChart />
-    </div>
-  </div>
-</div>
-
+        >
+          <h4 className="text-center font-semibold text-gray-800 mb-2">
+            Top Selling Products
+          </h4>
+          <div className="flex-1">
+            <SellerPieChart />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
