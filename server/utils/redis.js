7a544b5a87e64f5e6456a9a23f9f5a76e24cb377
SANDEEP NAUTIYAL <138180022+nautiyalsandeep19@users.noreply.github.com>
2025-12-12 13:36:51 +0530

    updated the redis in the company controller

---
+++ server/utils/redis.js
@@ -0,0 +1,17 @@
+import { createClient } from "redis";
+
+const client = createClient({
+  url: "redis://127.0.0.1:6379",
+});
+
+client.connect();
+
+client.on("connect", () => {
+  console.log("Redis connected");
+});
+
+client.on("error", (err) => {
+  console.error("Redis error:", err);
+});
+
+export default client;
