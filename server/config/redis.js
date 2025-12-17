2baa5a59587cae4bf6d52a0073a3e21aa60b5a3b
SANDEEP NAUTIYAL <138180022+nautiyalsandeep19@users.noreply.github.com>
2025-12-17 12:42:18 +0530

    updated the redis  cache and the redis utils

--- server/config/redis.js
+++ server/config/redis.js
@@ -1,16 +1,16 @@
-// import Redis from "ioredis";
+import Redis from "ioredis";
 
-// const redis = new Redis({
-//   host: "127.0.0.1",
-//   port: 6379,
-// });
+const redis = new Redis({
+  host: "127.0.0.1",
+  port: 6379,
+});
 
-// redis.on("connect", () => {
-//   console.log("✅ Redis connected");
-// });
+redis.on("connect", () => {
+  console.log("✅ Redis connected");
+});
 
-// redis.on("error", (err) => {
-//   console.error("❌ Redis error:", err);
-// });
+redis.on("error", (err) => {
+  console.error("❌ Redis error:", err);
+});
 
-// export default redis;
+export default redis;
