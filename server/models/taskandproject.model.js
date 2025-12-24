35f0dd97f67533fb91f22dbf457416a5d1c3b020
saurabhbisht95 <bishtsaurabh15@gmail.com>
2025-12-24 18:15:19 +0530

    strucutre taskandprojet model

--- server/models/taskandproject.model.js
+++ server/models/taskandproject.model.js
@@ -1,12 +1,21 @@
-import mongoose ,{ Schema } from 'mongoose';
+import mongoose, { Schema } from "mongoose";
 
-
-const TaskAndProjectSchema = new Schema({
-
-    title: { type: String, required: true  },
+const TaskAndProjectSchema = new Schema(
+  {
+    title: { type: String, required: true },
     description: { type: String },
-    status: { type: String, enum: ['To Do', 'Doing', 'Review', 'Done'], default: 'To Do' },
-    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, 
-},{ timestamps: true })
+    status: {
+      type: String,
+      enum: ["To Do", "Doing", "Review", "Done"],
+      default: "To Do",
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
-export default mongoose.model('TaskAndProject', TaskAndProjectSchema);
+export default mongoose.model("TaskAndProject", TaskAndProjectSchema);
